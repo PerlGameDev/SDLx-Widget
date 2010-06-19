a13ac5424a69143906b18a097ae7a59d729f1e52
Breno G. de Oliveira <garu@cpan.org>
2010-06-19 18:35:13 -0300

    sprite: making 04.pl show method chaining example

--- examples/04.pl
+++ examples/04.pl
@@ -15,14 +15,12 @@ my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT);
 
 my $sprite = SDLx::Widget::Sprite->new();
 
- $sprite->load('data/hero.png');
+$sprite->load('data/hero.png')->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff));
 
- $sprite->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff));
+$sprite->x(10);
+$sprite->y(10);
+$sprite->draw($disp);
 
- $sprite->x(10);
- $sprite->y(10);
- $sprite->draw($disp);
+SDL::Video::update_rect($disp, 0, 0, 300,300);
 
- SDL::Video::update_rect($disp, 0, 0, 300,300);
-
- SDL::delay( 1000 );
+SDL::delay( 1000 );
