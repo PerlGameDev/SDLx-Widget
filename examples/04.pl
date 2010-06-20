c1dcf5d2de3a3444298b942eb896cd0105436a7b
Breno G. de Oliveira <garu@cpan.org>
2010-06-20 04:42:54 -0300

    adding sprite rotation to the examples

--- examples/04.pl
+++ examples/04.pl
@@ -4,6 +4,7 @@ use SDL::Video;
 use SDL::Color;
 use SDL::Rect;
 use SDL::Surface;
+use SDL::GFX::Rotozoom;
 
 use SDLx::Widget::Sprite;
 
@@ -15,20 +16,25 @@ my $disp = SDL::Video::set_video_mode( 300, 300, 32, SDL_ANYFORMAT);
 
 my $sprite = SDLx::Widget::Sprite->new();
 
- $sprite->load('data/chest.png')->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff))->alpha(0xcc);
+ $sprite->load('data/chest.png');
+ $sprite->x(10);
+ $sprite->y(10);
 
- $sprite->x(0);
- $sprite->y(0);
- $sprite->draw($disp);
+ my $angle = 0;
+ while ($angle++ < 20) {
+     $sprite->rotation($angle);
+     $sprite->alpha_key(SDL::Color->new(0xfc, 0x00, 0xff))->alpha(0xcc);
 
- SDL::Video::update_rect($disp, 0, 0, 300,300);
- 
- SDL::delay( 300 );
+     $sprite->draw($disp);
 
- $sprite->alpha(0.20); 
- $sprite->draw_xy( $disp, 49, 49);
+     SDL::Video::update_rect($disp, 0, 0, 300,300);
 
- SDL::Video::update_rect($disp, 0, 0, 300,300);
+     SDL::delay( 100 );
 
+     $sprite->alpha(0.20);
+     $sprite->draw_xy( $disp, 60, 60);
+
+     SDL::Video::update_rect($disp, 0, 0, 300,300);
+ }
  SDL::delay( 2000 );
 
