1e8b45eb93ac351b1b8fa609ebad98920ed7a41a
kthakore <thakore.kartik@gmail.com>
2010-11-30 16:25:45 -0500

    Using Mouse for the text box now

--- lib/SDLx/Widget/Textbox.pm
+++ lib/SDLx/Widget/Textbox.pm
@@ -14,82 +14,86 @@ use Encode;
 use Clipboard;
 use Time::HiRes;
 
-sub new {
-    my $class          = shift;
-    my %params         = @_;
-    my $self           = \%params;
-    $self->{value}     = '';
-    $self->{focus}     = 0;
-    $self->{cursor}    = 0;
-    $self->{mousedown} = 0;
-    $self->{textbox}   = {};
+use Mouse;
+
+has 'app'   => ( is => 'ro', isa => 'SDLx::Controller', required => 1 );
+has 'value' => ( is => 'rw', isa => 'Str', default => '' );
+has 'focus' => ( is => 'rw', isa => 'Int', default => 0 );
+has 'cursor' => ( is => 'rw', isa => 'Int', default => 0 );
+has 'mousedown' => ( is => 'rw', isa => 'Int', default => 0 );
+has 'x'	=> ( is => 'rw', isa => 'Int' ,required => 1 );
+has 'y'	=> ( is => 'rw', isa => 'Int' ,required => 1 );
+has 'w'	=> ( is => 'rw', isa => 'Int' ,required => 1 );
+has 'h'	=> ( is => 'rw', isa => 'Int' ,required => 1 );
+has 'name'	=> ( is => 'rw', isa => 'Str', required => 1  );
+
+has 'textbox' => ( is => 'rw', isa => 'HashRef', default => sub{ {} } );
+sub BUILD {
+    my $self          = shift;
     $self->{textbox_render} = sub {
         my $_self = $self;
 		my $delta = $_[0];
-        $_self->{app}->draw_rect( [$_self->{x}, $_self->{y}, $_self->{w}, $_self->{h}], [255,255,255,255] );
+        $_self->app->draw_rect( [$_self->x, $_self->y, $_self->w, $_self->h], [255,255,255,255] );
         
         # calculation the text-highlight-box on mouse movement
         if($self->{mousedown} && $_self->{value}) {
             my ($mask, $mouse_x)      = @{ SDL::Events::get_mouse_state( ) };
-            my $text_end              = $_self->{x} + 2 + length($_self->{value}) * 8;
-            $mouse_x                  = $_self->{x} + $_self->{w} if $mouse_x > $_self->{x} + $_self->{w};
-            $mouse_x                  = $_self->{x}               if $mouse_x < $_self->{x};
+            my $text_end              = $_self->x + 2 + length($_self->{value}) * 8;
+            $mouse_x                  = $_self->x + $_self->w if $mouse_x > $_self->x + $_self->w;
+            $mouse_x                  = $_self->x               if $mouse_x < $_self->x;
             $mouse_x                  = $text_end                 if $mouse_x > $text_end;
             $_self->{mousedown}       = $text_end                 if $_self->{mousedown} > $text_end;
-            $_self->{selection_start} = int(($_self->{mousedown} - 2 - $_self->{x}) / 8 + 0.5);
-            $_self->{selection_stop}  = int(($mouse_x            - 2 - $_self->{x}) / 8 + 0.5);
+            $_self->{selection_start} = int(($_self->{mousedown} - 2 - $_self->x) / 8 + 0.5);
+            $_self->{selection_stop}  = int(($mouse_x            - 2 - $_self->x) / 8 + 0.5);
         }
         
         # drawing the text-highlight-box
         if(defined $_self->{selection_start} && defined $_self->{selection_stop} && $_self->{selection_start} != $_self->{selection_stop}) {
             ($_self->{selection_start}, $_self->{selection_stop}) = sort {$a <=> $b} ($_self->{selection_start}, $_self->{selection_stop});
             my $width = ($_self->{selection_stop} - $_self->{selection_start}) * 8;
-            my $left  = $_self->{selection_start} * 8 + 2 + $_self->{x};
-            $_self->{app}->draw_rect( [$left, $_self->{y} + 2, $width, $_self->{h} - 4], [128,128,255,255] );
+            my $left  = $_self->{selection_start} * 8 + 2 + $_self->x;
+            $_self->app->draw_rect( [$left, $_self->y + 2, $width, $_self->h - 4], [128,128,255,255] );
         }
         
         # drawing the name of the textbox in grey letters
         if($_self->{name} && !length($_self->{value}) && !$_self->{focus}) {
-            $_self->{app}->draw_gfx_text( [$_self->{x} + 3, $_self->{y} + 7], 0xAAAAAAFF, $_self->{name} );
+            $_self->app->draw_gfx_text( [$_self->x + 3, $_self->y + 7], 0xAAAAAAFF, $_self->{name} );
         }
         # drawing asterisk for password fields
         elsif($_self->{password}) {
-            $_self->{app}->draw_gfx_text( [$_self->{x} + 3, $_self->{y} + 7], 0x000000FF, '*' x length($_self->{value}) );
+            $_self->app->draw_gfx_text( [$_self->x + 3, $_self->y + 7], 0x000000FF, '*' x length($_self->{value}) );
         }
         # drawing the value
         else {
-            $_self->{app}->draw_gfx_text( [$_self->{x} + 3, $_self->{y} + 7], 0x000000FF, $_self->{value} );
+            $_self->app->draw_gfx_text( [$_self->x + 3, $_self->y + 7], 0x000000FF, $_self->{value} );
         }
        
         # drawing the blinking cursor
         if($_self->{focus} && ( int($delta*10) % 2)
         && (!defined $_self->{selection_start} || !defined $_self->{selection_stop} || $_self->{selection_start} == $_self->{selection_stop})) {
-            my $x = $_self->{x} + 2 + $_self->{cursor} * 8;
-            $_self->{app}->draw_line( [$x, $_self->{y} + 2], [$x, $_self->{y} + $_self->{h} - 4], 0x000000FF, 0 );
+            my $x = $_self->x + 2 + $_self->cursor * 8;
+            $_self->app->draw_line( [$x, $_self->y + 2], [$x, $_self->y + $_self->h - 4], 0x000000FF, 0 );
         }
-        $_self->{app}->update;
+        $_self->app->update;
     };
 
-    $self           = bless $self, $class;
-    $self->{app}->add_event_handler( sub{$self->event_handler(@_)} );
+    $self->app->add_event_handler( sub{$self->event_handler(@_)} );
     SDL::Events::enable_unicode(1);
-
-    return $self;
 }
 
 
 sub show {
     my $self = shift;
 
-	$self->{app}->add_show_handler( $self->{textbox_render}, $self );
+	$self->app->add_show_handler( $self->{textbox_render}, $self );
 }
 
 sub event_handler {
     my ($self, $event, $app) = @_;
     
     if(SDL_MOUSEMOTION == $event->type) {
-        if($self->{x} <= $event->motion_x && $event->motion_x < $self->{x} + $self->{w}
-        && $self->{y} <= $event->motion_y && $event->motion_y < $self->{y} + $self->{h}) {
+        if($self->x <= $event->motion_x && $event->motion_x < $self->x + $self->w
+        && $self->y <= $event->motion_y && $event->motion_y < $self->y + $self->h) {
             #warn "on_mousemotion";
         }
         if($self->{focus} && $self->{mousedown}) {
@@ -97,8 +101,8 @@ sub event_handler {
         }
     }
     elsif(SDL_MOUSEBUTTONDOWN == $event->type) {
-        if($self->{x} <= $event->button_x && $event->button_x < $self->{x} + $self->{w}
-        && $self->{y} <= $event->button_y && $event->button_y < $self->{y} + $self->{h}) {
+        if($self->x <= $event->button_x && $event->button_x < $self->x + $self->w
+        && $self->y <= $event->button_y && $event->button_y < $self->y + $self->h) {
             #warn "on_mousedown";
             if(SDL_BUTTON_LEFT == $event->button_button) {
                 if(!$self->{focus}) {
@@ -109,7 +113,7 @@ sub event_handler {
                     # single click
                     if(!defined $self->{lastclick} || Time::HiRes::time - $self->{lastclick} >= 0.3) {
                         #warn 'on_click';
-                        $self->{cursor} = int(($event->button_x - 2 - $self->{x}) / 8 + 0.5);
+                        $self->{cursor} = int(($event->button_x - 2 - $self->x) / 8 + 0.5);
                         $self->{cursor} = length($self->{value}) if $self->{cursor} > length($self->{value});
                         $self->{mousedown} = $event->button_x;
                     }
@@ -144,8 +148,8 @@ sub event_handler {
         }
     }
     elsif(SDL_MOUSEBUTTONUP == $event->type) {
-        if($self->{x} <= $event->button_x && $event->button_x < $self->{x} + $self->{w}
-        && $self->{y} <= $event->button_y && $event->button_y < $self->{y} + $self->{h}) {
+        if($self->x <= $event->button_x && $event->button_x < $self->x + $self->w
+        && $self->y <= $event->button_y && $event->button_y < $self->y + $self->h) {
             #warn "on_mouseup";
         }
         else {
--- t/001_load.t
+++ t/001_load.t
@@ -2,10 +2,12 @@
 
 # t/001_load.t - check module loading and create testing directory
 
-use Test::More tests => 1;
+use Test::More tests => 3;
 
 BEGIN { 
 	use_ok( 'SDLx::Widget' );
+	use_ok( 'SDLx::Widget::Menu' );
+	use_ok( 'SDLx::Widget::Textbox');
 
 }
 
